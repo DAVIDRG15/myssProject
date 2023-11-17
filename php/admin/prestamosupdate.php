18c8187a5eb73116b9b15c4c364a420544486452
Robledo009 <chuyf20021@gmail.com>
2023-11-17 10:12:03 -0600

    Update prestamosupdate.php

--- php/admin/prestamosupdate.php
+++ php/admin/prestamosupdate.php
@@ -5,15 +5,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $Folio = $_POST['Folio'];
     $fecha_devolucion = $_POST['fecha_devolucion'];
     $estatus = $_POST['estatus_prestamo'];
-
+    $codigoQuery = $conn->query("SELECT codigo_lib FROM prestamos WHERE Folio = '$Folio'");
+    $row = $codigoQuery->fetch_assoc();
+    $codigoLibro = $row['codigo_lib'];
     $sql = "UPDATE prestamos SET fecha_devolucion='$fecha_devolucion', estatus_prestamo='$estatus'WHERE Folio='$Folio'";
 
     if ($conn->query($sql) === TRUE) {
         if ($conn->affected_rows > 0) {
+            if($estatus==="DEVUELTO"){
+                $sqlUpdateCantidadp = "UPDATE libro SET cantidad = cantidad + 1 WHERE codigo_libro = '$codigoLibro'";
+                $conn->query($sqlUpdateCantidadp);
+            }
             echo "<script>alert('Prestamo actualizado correctamente'); window.location.href = 'prestamos.php';</script>";
             exit();
         } else {
-            echo "<script>alert('No se encontró un prestamo con el código \"$Folio\" para actualizar.'); window.location.href = 'prestamos.php';</script>";
+            echo "<script>alert('No se encontró un prestamo con el folio \"$Folio\" para actualizar.'); window.location.href = 'prestamos.php';</script>";
         }
     } else {
         echo "Error al actualizar prestamo: " . $conn->error;
