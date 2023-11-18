53e61838ac0a016034d27afcadabfd40b6bba7c0
Robledo009 <chuyf20021@gmail.com>
2023-11-18 02:23:14 -0600

    Correción(?)
    Tuve problemas en mi lap, puedes revisar si se ve bien todo?

--- php/conexion.php
+++ php/conexion.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "biblioteca";
+$servername = "blsn1oqors8cveyewvzf-mysql.services.clever-cloud.com";
+$username = "uyfsurxa5hm8bxrj";
+$password = "G79bzi0WlqdhAxiztxzE";
+$database = "blsn1oqors8cveyewvzf";
 
 $conn = new mysqli($servername, $username, $password, $database);
 
